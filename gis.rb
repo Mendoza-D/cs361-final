74e99e6dce6f756849a55fed8cbcd092294ffab3
Devin <mendozde@oregonstate.edu>
2024-12-07 21:33:34 -0800

    Refactored Track and TrackSegment to improve readability and align with SRP

--- gis.rb
+++ gis.rb
@@ -3,60 +3,33 @@
 class Track
   def initialize(segments, name=nil)
     @name = name
-    segment_objects = []
-    segments.each do |s|
-      segment_objects.append(TrackSegment.new(s))
+    @segments = segments.map { |s| TrackSegment.new(s) }
     end
-    # set segments to segment_objects
-    @segments = segment_objects
+
+    def to_geojson
+      {
+        "type": "Feature",
+        "properties": {title: @name}.compact,
+        "geometry": {
+          "type": "LineString",
+          "coordinates": @segments.map(&:coordinates_as_array) }
+      }
+    end
   end
 
-  def get_track_json()
-    j = '{'
-    j += '"type": "Feature", '
-    if @name != nil
-      j+= '"properties": {'
-      j += '"title": "' + @name + '"'
-      j += '},'
-    end
-    j += '"geometry": {'
-    j += '"type": "MultiLineString",'
-    j +='"coordinates": ['
-    # Loop through all the segment objects
-    @segments.each_with_index do |s, index|
-      if index > 0
-        j += ","
-      end
-      j += '['
-      # Loop through all the coordinates in the segment
-      tsj = ''
-      s.coordinates.each do |c|
-        if tsj != ''
-          tsj += ','
-        end
-        # Add the coordinate
-        tsj += '['
-        tsj += "#{c.lon},#{c.lat}"
-        if c.ele != nil
-          tsj += ",#{c.ele}"
-        end
-        tsj += ']'
-      end
-      j+=tsj
-      j+=']'
-    end
-    j + ']}}'
-  end
-end
 class TrackSegment
   attr_reader :coordinates
+
   def initialize(coordinates)
     @coordinates = coordinates
+  end
+
+  def coordinates_as_array
+    @coordinates.map { |c| [c.lon, c.lat, c.ele].compact }
   end
 end
 
 class Point
-
   attr_reader :lat, :lon, :ele
 
   def initialize(lon, lat, ele=nil)
@@ -67,9 +40,6 @@ class Point
 end
 
 class Waypoint
-
-
-
 attr_reader :lat, :lon, :ele, :name, :type
 
   def initialize(lon, lat, ele=nil, name=nil, type=nil)
@@ -80,30 +50,18 @@ attr_reader :lat, :lon, :ele, :name, :type
     @type = type
   end
 
-  def get_waypoint_json(indent=0)
-    j = '{"type": "Feature",'
-    # if name is not nil or type is not nil
-    j += '"geometry": {"type": "Point","coordinates": '
-    j += "[#{@lon},#{@lat}"
-    if ele != nil
-      j += ",#{@ele}"
-    end
-    j += ']},'
-    if name != nil or type != nil
-      j += '"properties": {'
-      if name != nil
-        j += '"title": "' + @name + '"'
-      end
-      if type != nil  # if type is not nil
-        if name != nil
-          j += ','
-        end
-        j += '"icon": "' + @type + '"'  # type is the icon
-      end
-      j += '}'
-    end
-    j += "}"
-    return j
+  def to_geojson
+    {
+      "type": "Feature",
+      geometry: {
+        "type": "Point",
+        "coordinates": [@lon, @lat, @ele].compact
+      },
+      "properties": {
+        title: @name,
+        type: @type
+      }.compact
+    }
   end
 end
 
@@ -116,26 +74,18 @@ end
     @features.append(t)
   end
 
-  def to_geojson(indent=0)
-    # Write stuff
-    s = '{"type": "FeatureCollection","features": ['
-    @features.each_with_index do |f,i|
-      if i != 0
-        s +=","
-      end
-        if f.class == Track
-            s += f.get_track_json
-        elsif f.class == Waypoint
-            s += f.get_waypoint_json
-      end
-    end
-    s + "]}"
+  def to_geojson
+    {
+      "type": "FeatureCollection",
+      "features": @features.map(&:to_geojson)
+    }
   end
 end
 
 def main()
   w = Waypoint.new(-121.5, 45.5, 30, "home", "flag")
   w2 = Waypoint.new(-121.5, 45.6, nil, "store", "dot")
+  
   ts1 = [
   Point.new(-122, 45),
   Point.new(-122, 46),
